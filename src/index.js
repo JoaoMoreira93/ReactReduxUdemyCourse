26357440a8651f866f771b67cadab06fb0e336a1
JoaoMoreira93 <31474686+JoaoMoreira93@users.noreply.github.com>
2017-10-25 22:57:34 +0100

    28. Handling Null Props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCFEeHdTdCZeLJqPyMrWzFQfVVbP2cPqhw';
 
@@ -23,6 +24,7 @@ class App extends Component{
     return (
     <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
     </div>
     );
