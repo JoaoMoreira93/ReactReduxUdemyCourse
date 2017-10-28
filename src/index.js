36baedeb0801095407d94f65f3a0e8069910d67e
JoaoMoreira93 <31474686+JoaoMoreira93@users.noreply.github.com>
2017-10-28 18:51:12 +0100

    Section 2 - Complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
@@ -13,19 +14,34 @@ class App extends Component{
   constructor(props){
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({ key: API_KEY, term: 'Maluco Beleza'}, (videos) =>{
-      this.setState({ videos });
-      // this.setState({ videos: videos}); <-- Key & prop same name
-    });
+    this.videoSearch("Maluco Beleza");
   }
+
+videoSearch(term){
+  YTSearch({ key: API_KEY, term: term}, (videos) =>{
+    this.setState({
+       videos: videos,
+       selectedVideo: videos[0]
+     });
+    // this.setState({ videos: videos}); <-- Key & prop same name
+  });
+}
+
   render(){
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
     <div>
-        <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
     </div>
     );
   }
