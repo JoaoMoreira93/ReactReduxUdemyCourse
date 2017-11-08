813529dc42978c5609d5d8e396238a4c1a21861c
JoaoMoreira93 <31474686+JoaoMoreira93@users.noreply.github.com>
2017-11-08 18:06:43 +0000

    54 - Form Elements in React

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,33 @@
 import React,{Component} from 'react';
 
 export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {term:''};
+        // Para definir o contexto do this desta função é necessário
+        // fazer o binding no construtor
+        this.onInputChange = this.onInputChange.bind(this);
+        //  this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event){
+        event.preventDefault();
+    }
+
     render(){
         return(
-            <form className="input-group">
-                <input />
+            <form className="input-group" onSubmit={this.onFormSubmit}>
+                <input 
+                    placeholder="Obter previsão semanal da sua cidade favorita!"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}/>
+
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
