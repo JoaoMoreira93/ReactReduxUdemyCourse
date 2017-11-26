5b15db3f3517449ab8cc102d2c630e45a16c9267
JoaoMoreira93 <31474686+JoaoMoreira93@users.noreply.github.com>
2017-11-26 02:23:23 +0000

    Form Validation and Styling

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,31 +1,42 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link} from 'react-router-dom';
 
 class PostsNew extends Component {
     renderField(field) {
+        const { meta:{ touched, error} } = field;
+        const className = `form-group ${touched && error ? 'has-danger':''}`;
         return (
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                    { field.meta.touched ? field.meta.error : ''}
+                </div>
             </div>
         );
     }
 
+
+    onSubmit(values){
+        console.log(values);
+    }
     render() {
+        const { handleSubmit} = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
                     component={this.renderField}
                 />
                 <Field
-                    label="Tags"
-                    name="tags"
+                    label="Categories"
+                    name="categories"
                     component={this.renderField}
                 />
                 <Field
@@ -33,11 +44,35 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}                    
                 />
+                <button type="submit"
+                        className="btn btn-primary">
+                        Submit</button>
+                
+                <Link to="/"
+                className="btn btn-danger">
+                Cancel</Link>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if(!values.title){
+        errors.title = "Enter a title!";        
+    }
+    if(!values.categories){
+        errors.categories = "Enter some categories!";
+    }
+    if(!values.content){
+        errors.content = "Enter a content!";
+    }
+    // if errors is empty -> no errors
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
