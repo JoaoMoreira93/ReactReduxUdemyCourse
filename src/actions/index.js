74d46f10ed06c63204a5d2af0fd64de1b12bb40b
JoaoMoreira93 <31474686+JoaoMoreira93@users.noreply.github.com>
2017-11-26 15:45:59 +0000

    End of React Router + Redux Form Section

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POSTS = 'create_posts';
+export const DELETE_POST = 'delete_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=AGUASLUSO123';
@@ -12,5 +15,36 @@ export function fetchPosts() {
     return {
         type: FETCH_POSTS,
         payload: req
+    };
+}
+
+export function createPost(values, callback) {
+    const req = axios.post(`${ROOT_URL}/posts${API_KEY}`, values).then(()=>{
+        callback();
+    });
+    
+    return {
+        type: CREATE_POSTS,
+        payload: req
+    };
+}
+
+export function fetchPost(id) {
+    const req = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+    
+    return {
+        type: FETCH_POST,
+        payload: req
+    };
+}
+
+export function deletePost(id, callback) {
+    axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`).then(()=>{
+        callback();
+    });
+    
+    return {
+        type: DELETE_POST,
+        payload: id
     };
 }
